67640450d2adada25abe810c0e851927a9dcb440
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-03-06 15:58:51 +0200

    amount

--- main.js
+++ main.js
@@ -267,19 +267,19 @@ Given a year report if it is a leap year
 // console.log(amountTocoins(46, [25, 10, 5, 2,1]));
 
 
-function amountTocoins(amount,coins){
-if(amount === 0){
-    return [];
-}
-else{
-    if(amount >= coins[0]){
-        left=amount-coins[0];
-        return [coins[0]].concat(amountTocoins(left,coins))
-    }
-    {
-        coins.shift();
-        return amountTocoins(amount, coins);
-       }
-}
-}
-console.log(amountTocoins(10,[3,2,1]))
+// function amountTocoins(amount,coins){
+// if(amount === 0){
+//     return [];
+// }
+// else{
+//     if(amount >= coins[0]){
+//         left=amount-coins[0];
+//         return [coins[0]].concat(amountTocoins(left,coins))
+//     }
+//     {
+//         coins.shift();
+//         return amountTocoins(amount, coins);
+//        }
+// }
+// }
+// console.log(amountTocoins(10,[3,2,1]))
